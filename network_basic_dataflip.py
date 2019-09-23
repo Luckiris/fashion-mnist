da44d50845fefc94a0f915a64888f43d2a3108d5
Arnaud "Luckiris <arnaud.mamecier@gmail.com>
2019-09-23 16:06:35 +0200

    Try to reduce the factor (FAIL)

--- network_basic_dataflip.py
+++ network_basic_dataflip.py
@@ -47,12 +47,12 @@ model.add(Dropout(0.25))
 model.add(BatchNormalization())
 model.add(Dense(128, activation='relu'))
 
-# model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-# model.add(Dense(128, activation='relu'))
-# model.add(BatchNormalization())
+model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Dense(128, activation='relu'))
+model.add(BatchNormalization())
 
 model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
 model.add(Conv2D(128, (3, 3), activation='relu'))
@@ -66,7 +66,7 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.01,
                               patience=5, min_lr=0.001)
 
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(),
