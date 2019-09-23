abbcc263c4006a063aa866625e89f74d6aeceba0
Arnaud "Luckiris <arnaud.mamecier@gmail.com>
2019-09-23 15:04:00 +0200

    Basic network 93 %
    The basic network (CNN) with version with and without horizontal flip

---
+++ network_basic_dataflip.py
@@ -0,0 +1,96 @@
+from __future__ import print_function
+
+from datetime import datetime
+from math import ceil
+
+import keras
+from keras.callbacks import ReduceLROnPlateau, TensorBoard
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras import backend as K
+from keras_preprocessing.image import ImageDataGenerator
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+datagen = ImageDataGenerator(horizontal_flip=True)
+
+batch_size = 128
+num_classes = 10
+epochs = 100
+img_rows, img_cols = 28, 28
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+
+model.add(Conv2D(64, kernel_size=(3, 3), padding='same', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(BatchNormalization())
+model.add(Dense(128, activation='relu'))
+
+model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Dense(128, activation='relu'))
+model.add(BatchNormalization())
+
+model.add(Conv2D(128, kernel_size=(3, 3), padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu'))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                              patience=5, min_lr=0.001)
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.summary()
+
+date = datetime.now().strftime("%d-%m-%Y - %H-%M-%S")
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(date))
+
+# model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_data=(x_test, y_test),
+#          callbacks=[reduce_lr, tensorboard])
+
+model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                    validation_data=(x_test, y_test),
+                    steps_per_epoch=len(x_train)//batch_size,
+                    epochs=epochs,
+                    verbose=2,
+                    callbacks=[reduce_lr, tensorboard])
+
+score = model.evaluate(x_test, y_test, verbose=2)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+model.save('Test_MNIST_CNN.h5')
